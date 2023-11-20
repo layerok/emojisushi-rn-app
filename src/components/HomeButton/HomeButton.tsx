46578e3941b9beca500ef8385c7a9149152a5ec2
INJOKER11 <67804388+INJOKER11@users.noreply.github.com>
2023-11-20 22:23:23 +0100

    feat: add home button component

--- src/components/HomeButton/HomeButton.tsx
+++ src/components/HomeButton/HomeButton.tsx
@@ -1,15 +1,35 @@
 import {Image, StyleSheet, Text, View} from 'react-native';
+import {nw, nh} from '../../../normalize.helper';
 const HomeButtonIcon = require('./home.png');
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+      container: {
+          width: nw(60),
+          height: nw(60),
+          backgroundColor: '#FFE600',
+          borderRadius: nw(60),
+          alignItems: 'center',
+          justifyContent: 'center'
+
+      },
+      img: {
+        width: nw(20),
+        height: nw(20)
+      },
+      shadowProp: {
+        shadowColor: '#FFE60080',
+        //shadowOffset: {width: -2, height: 2},
+        //shadowOpacity: 1,
+        //shadowRadius: 3,
+        elevation: 20,
+      }
+});
 
 export const HomeButton = () => {
   return (
     <View
-      style={{
-        backgroundColor: 'white',
-      }}>
-      <Image source={HomeButtonIcon} />
+      style={[styles.container, styles.shadowProp]}>
+      <Image source={HomeButtonIcon}  style={styles.img}/>
     </View>
   );
 };
